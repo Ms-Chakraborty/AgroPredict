94bf9d2ebda1f39827578b1b6b8a6622655ed27e
Ms-Chakraborty <138107297+Ms-Chakraborty@users.noreply.github.com>
2024-07-29 11:42:45 +0530

    git commit

---
+++ AgroPredict/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        chatblack: {50: ' #646f0e '}
+      }
+    },
+  },
+  plugins: [],
+}
+
